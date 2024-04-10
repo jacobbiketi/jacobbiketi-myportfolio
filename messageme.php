b72e9113c4c3385abe93bf1383fc913535523af8
jacobbiketi <jacobbiketi2023@gmail.com>
2024-04-10 18:14:36 +0300

    email

--- messageme.php
+++ messageme.php
@@ -1,27 +1,45 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
-    // Email address where you want to receive the message
-    $to = 'your_email@example.com';
+    // Establish connection to MySQL database
+    $servername = "localhost"; //MySQL server name
+    $username = "id21987217_myportfoliojacobbiketi"; // MySQL username
+    $password = "Myportfoliojacobbiketi2023>"; //  MySQL password
+    $dbname = "id21987217_myportfoliojacobbiketi"; // MySQL database name
 
-    $headers = "From: $name <$email>" . "\r\n";
-    $headers .= "Reply-To: $email" . "\r\n";
-    $headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    $body = "Name: $name <br>";
-    $body .= "Email: $email <br>";
-    $body .= "Subject: $subject <br>";
-    $body .= "Message: <br> $message";
-
-    // Send email
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Email sent successfully!";
-    } else {
-        echo "Failed to send email!";
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
+
+    // Prepare SQL statement to insert data into a table
+    $sql = "INSERT INTO message_me (name, email, subject, message) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssss", $name, $email, $subject, $message);
+
+    // Execute the SQL statement
+    if ($stmt->execute()) {
+        // Close prepared statement and database connection
+        $stmt->close();
+        $conn->close();
+        echo "<script>alert('Email Submitted, Kindly wait for response. Thank you'); window.location.href = 'index.html';</script>";
+        exit;
+    } else {
+        echo "Email not sent! Error: " . $stmt->error;
+    }
+
+    // Close prepared statement and database connection
+    $stmt->close();
+    $conn->close();
 }
 ?>
